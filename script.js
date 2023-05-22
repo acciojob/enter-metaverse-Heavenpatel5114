7e1f622e5454877b1c7e5ba36482860a9ec2b33c
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-05-22 16:38:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const btn = document.getElementById("enterBtn");
+const para = document.getElementById("status")
+btn.addEventListener('click',()=>{
+  const newheading = document.createElement('h1');
+	newHeading.id = para.id;
+  newheading.textContent = para.textContent;
+  para.parentNode.replaceChild(newheading,para);
+	 
+})
