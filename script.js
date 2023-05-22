b5ca93333a9f1a3fde36978cb4c8d4bd45420910
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-05-22 16:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//your JS code here. If required.
-const btn = document.getElementById("enterBtn");
-const para = document.getElementById("status")
-btn.addEventListener('click',()=>{
-  const newheading = document.createElement('h1');
-	newHeading.id = para.id;
-  newheading.textContent = para.textContent;
-  para.parentNode.replaceChild(newheading,para);
-	 
-})
+const p = document.getElementById("status")
+const btn = document.getElementById("enterBtn")
+const body = document.querySelector("body")
+function changeText(){
+    const h1 = document.createElement("h1")
+    h1.innerText = "Entered Metaverse"
+    p.innerHTML = ""
+	p.appendChild(h1)
+}
+btn.addEventListener("click", changeText)
